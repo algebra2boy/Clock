a9219865f1d44f6cdd4947d167f20e1e9332c47c
Yongye Tan <yongyetan1209@gmail.com>
2024-11-03 21:26:42 -0500

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Yongye on 11/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
